df5d6dc327260c7d1947b62f43b806fce9923519
Luke Blakey <luke.blakey@vonage.com>
2022-02-03 14:42:57 +0000

    add template.sh

---
+++ template.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+usage() {
+    echo "usage: $0 -f FLAG [ -o OPTIONAL_FLAG ]"
+    exit 2
+}
+
+while getopts ':f:o' opt; do
+    case "$opt" in
+        f) FLAG="$OPTARG" ;;
+        o) OPTIONAL_FLAG="$OPTARG" ;;
+        \?) usage ;;
+        :) usage ;;
+    esac
+done
+
+[ "$FLAG" ] || usage
